6a6818f4f5a3d5642c9a3319e0ca841ab72fffc2
bearkid6 <148352113+bearkid6@users.noreply.github.com>
2024-03-20 00:46:28 +0800

    Update write lyrics.py

--- SourceCode/write lyrics.py
+++ SourceCode/write lyrics.py
@@ -1,11 +1,4 @@
-'''
-Author: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
-Date: 2024-03-20 00:00:42
-LastEditors: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
-LastEditTime: 2024-03-20 00:38:56
-FilePath: \fuck-python-in-school\SourceCode\write lyrics.py
-Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
-'''
+
 '''
                        _oo0oo_
                       o8888888o
