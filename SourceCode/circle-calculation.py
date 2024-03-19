69e92e12102fa1d00d1839bfae6d77d0663f5a0f
bearkid6 <148352113+bearkid6@users.noreply.github.com>
2024-03-20 00:56:19 +0800

    Update circle-calculation.py

--- SourceCode/circle-calculation.py
+++ SourceCode/circle-calculation.py
@@ -1,11 +1,4 @@
-'''
-Author: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
-Date: 2024-03-19 21:26:31
-LastEditors: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
-LastEditTime: 2024-03-20 00:01:18
-FilePath: \fuck-python-in-school\0-use.py
-Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
-'''
+
 #终端写圆
 a = float(input('please put a fucking nummber here:'))
 PI = 3.14
