e3e9bc05ebc4914f1f7fbf84a6a4c3cac805ceac
bearkid6 <148352113+bearkid6@users.noreply.github.com>
2024-03-20 13:08:46 +0800

    Add files via upload

--- SourceCode/circle-calculation.py
+++ SourceCode/circle-calculation.py
@@ -1,8 +1,49 @@
-
+'''
+Author: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
+Date: 2024-03-19 21:26:31
+LastEditors: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
+LastEditTime: 2024-03-20 13:06:47
+FilePath: \fuck-python-in-school\0-use.py
+Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+'''
 #终端写圆
-a = float(input('please put a fucking nummber here:'))
-PI = 3.14
-FuckC = a*2*PI
-FuckS = a**2*PI
-print('周长是',FuckC)
-print('面积是',FuckS)
+from time import sleep
+from threading import Thread
+import os
+import time
+sleep(0.5)
+
+PI = 3.14    
+a = input('please put a fucking nummber here:')
+b =a.isdigit()
+if  b == True :
+    a = float(a)
+    FuckC = a*2*PI
+    FuckS = a**2*PI
+    print('周长是',FuckC)
+    print('面积是',FuckS)
+else:
+    
+    sleep(0.3)
+    print('...')
+    sleep(0.5)
+    print('你滴,大大滴坏!!!')
+    sleep(0.5)
+    c = input('在这里打666就不把你电脑炸了-->')
+    d =int(c)
+    
+    if d == 666 :
+     print('这次就饶了你')
+    
+    else:
+       sleep(0.5)
+       i = 0
+       while i < 60:
+         i += 1
+         print('开炮!!!')
+         os.system("start cmd")
+         sleep(0.5)   
+                 
+       
+
+
