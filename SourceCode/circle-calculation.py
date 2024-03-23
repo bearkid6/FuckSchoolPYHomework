7b0d36ec8719a73025bead5643501691548ae0f3
bearkid6 <148352113+bearkid6@users.noreply.github.com>
2024-03-23 23:50:19 +0800

    Add files via upload

--- SourceCode/circle-calculation.py
+++ SourceCode/circle-calculation.py
@@ -2,26 +2,50 @@
 Author: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
 Date: 2024-03-19 21:26:31
 LastEditors: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
-LastEditTime: 2024-03-20 13:18:50
+LastEditTime: 2024-03-23 23:29:23
 FilePath: \fuck-python-in-school\0-use.py
 Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 '''
 #终端写圆
 from time import sleep
-from threading import Thread
+
 import os
 import time
-sleep(0.5)
+from tqdm import tqdm
 
+
+
+sleep(0.5)
+print('','')
+print('','')
+print('','')
 PI = 3.14    
 a = input('please put a fucking nummber here:')
-b =a.isdigit()
-if  b == True :
+b1 = False
+try:
+    b = float(a)
+    b1 = isinstance(b,float)
+except ValueError:
+   pass
+
+if  b1 == True : 
+ # happly ending
     a = float(a)
     FuckC = a*2*PI
     FuckS = a**2*PI
+    print('正在计算中')
+    
+    #等待几秒更有感觉
+    for i in tqdm(range(3, 10)):
+        sleep(0.1)
+    sleep(1)
+    
     print('周长是',FuckC)
     print('面积是',FuckS)
+    sleep(1)
+    print('---------------------------')
+    print('10秒后自动关闭')
+    sleep(10)
 else:
     
     sleep(0.3)
@@ -33,9 +57,15 @@ else:
     d =c.isdigit()
     
     if d == True and c == '666'  :
+     # still  a decent ending
+     sleep(0.4)
      print('这次就饶了你')
-    
+     sleep(0.4)
+     print('-------------------------')
+     print('15秒后自动关闭')
+     sleep(15)
     else:
+       #you die or i die
        sleep(0.5)
        i = 0
        while i < 60:
@@ -43,7 +73,9 @@ else:
          print('开炮!!!')
          os.system("start cmd")
          sleep(0.1)   
-#如若报错,请检查是否下载三方库 os , threading 和 time.
-       
 
+#如若报错,请检查是否下载三方库 os , threading ,tqdm和 time.
+#下载指令  'pip install XXX'  <--cmd中运行该命令  -> eg: T>pip install os  
+#如果实在不清楚为何报错,可以联系作者 "https://github.com/bearkid6/FuckSchoolPYHomework"
+#打不开请百度一下 '为什么打不开"GitHub"'
 
--- SourceCode/write-lyrics.py
+++ SourceCode/write-lyrics.py
@@ -1,4 +1,11 @@
-
+'''
+Author: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
+Date: 2024-03-20 00:00:42
+LastEditors: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
+LastEditTime: 2024-03-20 18:14:15
+FilePath: \fuck-python-in-school\SourceCode\write lyrics.py
+Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+'''
 '''
                        _oo0oo_
                       o8888888o
@@ -36,22 +43,22 @@
 '''
 #输出歌词
 from time import sleep
+import os
+from tqdm import tqdm
 
-
-sleep(0.2)
+sleep(1)
 print('歌词准备打印')
-
-
+sleep(1)
+print('---------------------')
+sleep(0.5)
+print('歌词打印中')
+    #文本
 l1 = "Soldier keep on marchin' on\nHead down 'til the work is done\nWaiting on that morning sun\nSoldier keep on marchin' on\n"
 l2 = "Head in the dust, feet in the fire\nLabour on that midnight wire\nListening for that angel choir\nYou got nowhere to run\nYou wanna take a drink of that\npromise land\n"
 l3 = "You gotta wipe the dirt off of\nyour hands\nCareful son, you got dreamer'splans\nBut it gets hard to stand\n"
 l4 = "Soldier keep on marchin' on\nHead down 'til the work is done\nWaiting on that morning sun"
 
-    #调用VIP进度条
-
-
-from tqdm import tqdm
-
+    #调用10秒VIP进度条
 for i in tqdm(range(1, 100)):
 
 
@@ -64,10 +71,27 @@ with open('FuckLyrics.txt', 'a') as file:
    
     file.writelines([l1, l2, l3, l4])
     file.flush()
-    #end
+    
 sleep(0.2)
-print('歌词已经打印到"FuckLyrics.txt"中')
+print('自动检查中.....')
 sleep(1)
 
-
+# 获取目录
+current_dir = os.getcwd()
+# 组合路径
+file_path = os.path.join(current_dir, 'FuckLyrics.txt')
+# 检查文件是否生成
+if os.path.exists(file_path):
+    print('打印成功，路径为：', file_path)
+else:
+    print('打印失败,请重试')
+#end
+sleep(2)
+print('-------------------------------------')
+print('10秒后自动关闭')
+sleep(10)
+#如若报错,请检查是否下载三方库 tqdm ,os 和 time.
+#下载指令  'pip install XXX'  <--cmd中运行该命令 -> eg: T>pip install time
+#如果实在不清楚为何报错,可以联系作者 "https://github.com/bearkid6/FuckSchoolPYHomework"
+#打不开请百度一下 '为什么打不开"GitHub"'
  
